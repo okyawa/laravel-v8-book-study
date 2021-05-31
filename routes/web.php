bc04100554bba47458144be4696ec5d5ed73aa17
Hirokazu Okawa <h.okyawa@gmail.com>
2021-06-01 06:33:45 +0900

    1-3-5: ユーザー登録の実装

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,3 +21,6 @@ Route::get('/', function () {
 Route::get('/home', function() {
     return view('home');
 });
+
+Route::get('/register', [RegisterController::class, 'create']);
+Route::post('/register', [RegisterController::class, 'store']);
