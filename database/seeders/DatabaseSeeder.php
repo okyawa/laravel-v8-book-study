1e958e89675478afc5993736ce145ef9968ec3ac
Hirokazu Okawa <h.okyawa@gmail.com>
2021-06-11 06:10:48 +0900

    5-2-2: シーダークラスを利用するための設定

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        // Authorsテーブルにデータ登録を行う処理
+        $this->call(AuthorsTableSeeder::class);
     }
 }
