1669f2ae5a8bc338497a9c37eebd78a6e669e983
Hirokazu Okawa <h.okyawa@gmail.com>
2021-06-11 05:07:46 +0900

    4-3-2: リソースクラスを組み合わせた REST APIレスポンスパターン

---
+++ app/Http/Resources/CommentResource.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+use function sprintf;
+
+class CommentResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->resource['id'],
+            'body' => $this->resource['body'],
+            '_links' => [
+                'self' => [
+                    'href' => sprintf(
+                        'https://example.com/comments/%s',
+                        $this->resource['id']
+                    )
+                ]
+            ],
+            '_embedded' => [
+                'user' => new UserResource(
+                    [
+                        'user_id' => $this->resource['user_id'],
+                        'user_name' => $this->resource['user_name']
+                    ]
+                )
+            ]
+        ];
+    }
+}
