cef5b7e225fc28f40d83600d94d1be0bb63c2ca5
Hirokazu Okawa <h.okyawa@gmail.com>
2021-07-02 06:28:06 +0900

    6-4-3: 動作拡張 - SocialiteのOAuth認証

--- app/Http/Controllers/Register/CallbackAction.php
+++ app/Http/Controllers/Register/CallbackAction.php
@@ -6,11 +6,17 @@ namespace App\Http\Controllers\Register;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use GuzzleHttp\Client;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\MessageFormatter;
+use GuzzleHttp\Middleware;
 use Illuminate\Auth\AuthManager;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Laravel\Socialite\Contracts\Factory;
 use Laravel\Socialite\Facades\Socialite;
+use Laravel\Socialite\Two\GithubProvider;
+use Psr\Log\LoggerInterface;
 
 /**
  * Socialiteを利用したユーザー作成とログイン
@@ -21,11 +27,37 @@ final class CallbackAction extends Controller
 {
     public function __invoke(
         Factory $factory,
-        AuthManager $authManager
+        AuthManager $authManager,
+        LoggerInterface $log
     ) {
         // コールバック時にSocialiteのメソッドを介してユーザー情報を取得
         // ここで返却されるメソッドは、抽象クラスのLaravel\Socialite\AbstractUserクラスを継承している
-        $user = $factory->driver('github')->user();
+        // $user = $factory->driver('github')->user();
+
+        /**
+         * 外部サービスとの通信内容をログとして出力する例
+         *
+         * GuzzleのMiddlewareを使ってフレームワークのログ出力に追加
+         * Socialiteで任意のGuzzleインスタンスを利用するには、
+         * setHttpClientメソッドが利用できる
+         * リスト 6.4.3.1
+         */
+        /** @var GithubProvider */
+        $driver = $factory->driver('github');
+        // リスト 6.4.3.2: GETリスエスト送信時のパラメータ追加例
+        $driver->with(['allow_signup' => 'false']);
+        // リスト 6.4.3.3: セッションを利用する例 (アプリケーション側で状態保持が必要な場合)
+        $driver->stateless()->user();
+        $user = $driver->setHttpClient(
+            new Client([
+                'handler' => tap(
+                    HandlerStack::create(),
+                    function (HandlerStack $stack) use ($log) {
+                        $stack->push(Middleware::log($log, new MessageFormatter()));
+                    }
+                )
+            ])
+        )->user();
 
         // 外部サービスから取得したユーザー情報をデータベースに登録し、ログイン処理を実行
         // login()メソッドの第2引数にtrueを指定し、自動ログインのクッキーを発行
