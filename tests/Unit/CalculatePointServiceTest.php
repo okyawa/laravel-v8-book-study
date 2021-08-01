c5e41ccf569b1bb19d4bbdbdbaf24570ff4f0c65
Hirokazu Okawa <h.okyawa@gmail.com>
2021-08-02 05:45:44 +0900

    9-1-5: 例外のテスト

--- tests/Unit/CalculatePointServiceTest.php
+++ tests/Unit/CalculatePointServiceTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit;
 
+use App\Exceptions\PreconditionException;
 use App\Services\CalculatePointService;
 use PHPUnit\Framework\TestCase;
 
@@ -69,5 +70,63 @@ class CalculatePointServiceTest extends TestCase
             '購入金額が9999なら99ポイント  ' => [99, 9999],
             '購入金額が10000なら200ポイント' => [200, 10000],
         ];
+    }
+
+    /**
+     * try/catchの利用
+     *
+     * リスト 9.1.5.1
+     *
+     * @test
+     */
+    public function exception_try_catch()
+    {
+        try {
+            throw new \InvalidArgumentException('message', 200);
+            $this->fail(); // 例外がスローされない場合はテストを失敗させる
+        } catch (\Throwable $e) {
+            // 指定した例外クラスがスローされているか
+            $this->assertInstanceOf(\InvalidArgumentException::class, $e);
+            // スローされた例外のコードを検証
+            $this->assertSame(200, $e->getCode());
+            // スローされた例外のメッセージを検証
+            $this->assertSame('message', $e->getMessage());
+        }
+    }
+
+    /**
+     * expectExceptionメソッドを使った例外のテスト
+     *
+     * try/catchを使う形式よりも、専用メソッドのexpectExceptionメソッドを使う形式の方が、
+     * 例外を期待するテストであるという意図が明確で分かりやすい
+     * リスト 9.1.5.2
+     *
+     * @test
+     */
+    public function exception_expectException_method()
+    {
+        // 指定した例外クラスがスローされているか
+        $this->expectException(\InvalidArgumentException::class);
+        // スローされた例外のコードを検証
+        $this->expectExceptionCode(200);
+        // スローされた例外のメッセージを検証
+        $this->expectExceptionMessage('message');
+
+        throw new \InvalidArgumentException('message', 200);
+    }
+
+    /**
+     * expectExceptionメソッドを使って購入金額が負数の場合のテスト
+     *
+     * リスト 9.1.5.3
+     *
+     * @test
+     */
+    public function calcPoint_購入金額が負の数なら例外をスロー()
+    {
+        $this->expectException(PreconditionException::class);
+        $this->expectExceptionMessage('購入金額が負の数');
+
+        CalculatePointService::calcPoint(-1);
     }
 }
