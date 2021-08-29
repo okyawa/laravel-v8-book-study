02afbe695979651b71761a9b4fb46e41fc2dad3f
Hirokazu Okawa <h.okyawa@gmail.com>
2021-08-30 05:37:17 +0900

    9-3-1: WebAPIテスト機能 - HTTPレスポンスのアサーション

--- tests/Feature/Api/PingTest.php
+++ tests/Feature/Api/PingTest.php
@@ -53,4 +53,82 @@ class PingTest extends TestCase
             'password' => 'secret-password'
         ]);
     }
+
+    /**
+     * jsonメソッドの定義
+     *
+     * リスト 9.3.1.7
+     */
+    /*
+    public function json(
+        $method,
+        $uri,
+        array $data = [],
+        array $headers = []
+    ): \Illuminate\Testing\TestResponse;
+    */
+
+    /**
+     * jsonメソッドで設定されているリクエストヘッダ
+     *
+     * Content-Length   JSONデータサイズ (byte)
+     * Content-Type     application/json
+     * Accept           application/json
+     *
+     * 表 9.3.1.8
+     */
+
+    /**
+     * 主なHTTPリクエストシミュレートメソッド
+     *
+     * callメソッドやjsonメソッドには、HTTPメソッドごとにラップしたメソッド、ラッパーメソッドが用意されている
+     * 通常はラッパメソッドを利用して、細かなパラメータ指定が必要な場合にはcallメソッドやjsonメソッドを利用するのが良い
+     *
+     * get($uri, array $headers = [])                           GETリクエスト
+     * getJson($uri, array $headers = [])                       GETリクエスト (JSON)
+     * post($uri, array $data = [], array $headers = [])        POSTリクエスト
+     * postJson($uri, array $data = [], array $headers = [])    POSTリクエスト (JSON)
+     * put($uri, array $data = [], array $headers = [])         PUTリクエスト
+     * putJson($uri, array $data = [], array $headers = [])     PUTリクエスト (JSON)
+     * patch($uri, array $data = [], array $headers = [])       PATCHリクエスト
+     * patchJson($uri, array $data = [], array $headers = [])   PATCHリクエスト (JSON)
+     * delete($uri, array $data = [], array $headers = [])      DELETEリクエスト
+     * deleteJson($uri, array $data = [], array $headers = [])  DELETEリクエスト (JSON)
+     *
+     * 表 9.3.1.9
+     */
+
+    /**
+     * HTTPレスポンスのアサーション (Illuminate\Testing\TestResponse)
+     *
+     * 送信したHTTPリクエストに対するHTTPレスポンスを検証するアサーションメソッドが用意されている
+     * Tests\TestCaseクラスよりも、HTTPリクエスト送信メソッドの戻り値であるIlluminate\Testing\TestResponseの方が、
+     * 送信したリクエストに特化したアサーションの記述が用意なので、こちらを利用する
+     * HTTPステータスコードとレスポンスボディの検証を主に、必要であればヘッダやクッキーの検証を実施すると良い
+     *
+     * assertStatus($status)                    HTTPステータスコードが$statusに一致していれば成功
+     * assertSuccessful()                       HTTPステータスコードが2xxなら成功
+     * assertRedirect($uri = null)              HTTPステータスコードが201,301,302,303,307,308のいずれかで、かつLocationヘッダの値がapp('url')->to($url)の値と一致すれば成功
+     * assertHeader($headerName, $value = null) レスポンスヘッダが存在($valueがnullの場合)、もしくは該当のレスポンスヘッダの値が$valueと一致すれば成功
+     * assertHeaderMissing($headerName)         指定したレスポンスヘッダが存在しなければ成功
+     * assertExactJson(array $data)             レスポンスボディのJSONをデコードした配列が$dataと一致すれば成功
+     * assertJson($value, $strict = false)      レスポンスボディのJSONをデコードした配列に$dataが含まれていれば成功
+     *
+     * 表 9.3.1.10
+     */
+
+    /**
+     * PingTestクラスにping APIのテストを実装
+     *
+     * リスト 9.3.1.11
+     * @test
+     */
+    public function get_ping()
+    {
+        $response = $this->get('/api/ping');
+        // HTTPステータスコードを検証
+        $response->assertStatus(200);
+        // レスポンスボディのJSONを検証
+        $response->assertExactJson(['message' => 'pong']);
+    }
 }
